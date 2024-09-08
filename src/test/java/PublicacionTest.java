af6b966343d396e64fc4e1e642115b9c45ab13ea
xnocc <alonsomikoulits@gmail.com>
2024-09-07 22:55:03 -0500

    terminado PublicacionTest.java

--- src/test/java/PublicacionTest.java
+++ src/test/java/PublicacionTest.java
@@ -1,3 +1,4 @@
+import com.mycompany.biblioteca.java.Publicacion;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -6,14 +7,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PublicacionTest {
-    // TODO Adiciona tus pruebas unitarias aquí.
-    // Los métodos deben estar anotados con la anotación @Test. Por ejemplo:
-    //
-//    @Test
-//    public void testMultiplicarPositivos() {
-//        int valorEsperado = 15;
-//        CuentaBancaria miCuenta = new CuentaBancaria();
-//        int valorActual = miCuenta.multiplicar(3, 5);
-//        assertEquals(valorEsperado, valorActual);
-//    }
+    
+    @Test
+    public void testSetTitulo() {
+        Publicacion publicacion = new Publicacion();
+        publicacion.setTitulo("Brayan");
+        assertEquals("Brayan", publicacion.getTitulo());
+    }
+
+    @Test
+    public void testSetAnoPublicacion() {
+        Publicacion publicacion = new Publicacion();
+        publicacion.setAnoPublicacion(2020);
+        assertEquals(2020, publicacion.getAnoPublicacion());
+    }
+    
+    
 }
